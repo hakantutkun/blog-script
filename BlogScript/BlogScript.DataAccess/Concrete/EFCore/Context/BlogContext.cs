0ff701a8bc94dd81b77e5e67cec3fd6f7d19e276
Hakan Tutkun <hakantutkun@gmail.com>
2021-05-11 03:11:22 +0300

    DataAccess katmanı oluşturuldu. Context ve GenericRepository eklendi.

---
+++ BlogScript/BlogScript.DataAccess/Concrete/EFCore/Context/BlogContext.cs
@@ -0,0 +1,22 @@
+﻿using BlogScript.Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BlogScript.DataAccess.Concrete.EFCore.Context
+{
+    public class BlogContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"server=.\SQLEXPRESS;database=BlogScript; integrated security=true;");
+        }
+        public DbSet<AppUser> AppUsers { get; set; }
+        public DbSet<Blog> Blogs { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<CategoryBlog> CategoryBlogs { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+
+    }
+}
---
+++ BlogScript/BlogScript.DataAccess/Concrete/EFCore/Repositories/EFGenericRepository.cs
@@ -0,0 +1,67 @@
+﻿using BlogScript.DataAccess.Abstract;
+using BlogScript.DataAccess.Concrete.EFCore.Context;
+using BlogScript.Entities.Abstract;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogScript.DataAccess.Concrete.EFCore.Repositories
+{
+    public class EFGenericRepository<TEntity> : IGenericDal<TEntity> where TEntity : class, IBlogEntity, new()
+    {
+        public async Task<List<TEntity>> GetAllAsync()
+        {
+            using var context = new BlogContext();
+            return await context.Set<TEntity>().ToListAsync();
+        }
+
+        public async Task<List<TEntity>> GetAllAsync(Expression<Func<TEntity, bool>> filter)
+        {
+            using var context = new BlogContext();
+            return await context.Set<TEntity>().ToListAsync();
+        }
+
+        public async Task<List<TEntity>> GetAllAsync<TKey>(Expression<Func<TEntity, bool>> filter, Expression<Func<TEntity, TKey>> keySelector)
+        {
+            using var context = new BlogContext();
+            return await context.Set<TEntity>().Where(filter).ToListAsync();
+        }
+
+        public async Task<List<TEntity>> GetAllAsync<TKey>(Expression<Func<TEntity, TKey>> keySelector)
+        {
+            using var context = new BlogContext();
+            return await context.Set<TEntity>().OrderByDescending(keySelector).ToListAsync();
+        }
+
+        public async Task<TEntity> GetAsync(Expression<Func<TEntity, bool>> filter)
+        {
+            using var context = new BlogContext();
+            return await context.Set<TEntity>().FirstOrDefaultAsync(filter);
+        }
+
+        public async Task AddAsync(TEntity entity)
+        {
+            using var context = new BlogContext();
+            await context.AddAsync(entity);
+            await context.SaveChangesAsync();
+        }
+
+        public async Task RemoveAsync(TEntity entity)
+        {
+            using var context = new BlogContext();
+            context.Remove(entity);
+            await context.SaveChangesAsync();
+        }
+
+        public async Task UpdateAsync(TEntity entity)
+        {
+            using var context = new BlogContext();
+            context.Update(entity);
+            await context.SaveChangesAsync();
+        }
+    }
+}
