6a94f10e38bb3c6f60a8f5142323963c2bf3b808
Hakan Tutkun <hakantutkun@gmail.com>
2021-05-12 02:27:12 +0300

    Business katmanı oluşturuldu. IGenericService ve GenericManager eklendi.

---
+++ BlogScript/BlogScript.Business/Abstract/IGenericService.cs
@@ -0,0 +1,21 @@
+﻿using BlogScript.Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogScript.Business.Abstract
+{
+    public interface IGenericService<TEntity> where TEntity : class, IBlogEntity, new()
+    {
+        Task<List<TEntity>> GetAllAsync();
+        Task<List<TEntity>> GetAllAsync(Expression<Func<TEntity, bool>> filter);
+        Task<List<TEntity>> GetAllAsync<TKey>(Expression<Func<TEntity, bool>> filter, Expression<Func<TEntity, TKey>> keySelector);
+        Task<List<TEntity>> GetAllAsync<TKey>(Expression<Func<TEntity, TKey>> keySelector);
+        Task<TEntity> GetAsync(Expression<Func<TEntity, bool>> filter);
+        Task AddAsync(TEntity entity);
+        Task UpdateAsync(TEntity entity);
+        Task RemoveAsync(TEntity entity);
+    }
+}
---
+++ BlogScript/BlogScript.Business/Concrete/GenericManager.cs
@@ -0,0 +1,61 @@
+﻿using BlogScript.Business.Abstract;
+using BlogScript.DataAccess.Abstract;
+using BlogScript.Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogScript.Business.Concrete
+{
+    class GenericManager<TEntity> : IGenericService<TEntity> where TEntity : class, IBlogEntity, new()
+    {
+        private readonly IGenericDal<TEntity> _genericDal;
+
+        public GenericManager(IGenericDal<TEntity> genericDal)
+        {
+            _genericDal = genericDal;
+        }
+
+        public async Task<List<TEntity>> GetAllAsync()
+        {
+            return await _genericDal.GetAllAsync();
+        }
+
+        public async Task<List<TEntity>> GetAllAsync(Expression<Func<TEntity, bool>> filter)
+        {
+            return await _genericDal.GetAllAsync(filter);
+        }
+
+        public async Task<List<TEntity>> GetAllAsync<TKey>(Expression<Func<TEntity, bool>> filter, Expression<Func<TEntity, TKey>> keySelector)
+        {
+            return await _genericDal.GetAllAsync(filter, keySelector);
+        }
+
+        public async Task<List<TEntity>> GetAllAsync<TKey>(Expression<Func<TEntity, TKey>> keySelector)
+        {
+            return await _genericDal.GetAllAsync(keySelector);
+        }
+
+        public async Task<TEntity> GetAsync(Expression<Func<TEntity, bool>> filter)
+        {
+            return await _genericDal.GetAsync(filter);
+        }
+
+        public async Task AddAsync(TEntity entity)
+        {
+            await _genericDal.AddAsync(entity);
+        }
+
+        public async Task RemoveAsync(TEntity entity)
+        {
+            await _genericDal.RemoveAsync(entity);
+        }
+
+        public async Task UpdateAsync(TEntity entity)
+        {
+            await _genericDal.UpdateAsync(entity);
+        }
+    }
+}
