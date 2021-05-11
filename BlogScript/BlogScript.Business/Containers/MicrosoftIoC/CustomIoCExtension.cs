8440c9d6a7280cdc5eb889d239bd950d4292a468
Hakan Tutkun <hakantutkun@gmail.com>
2021-05-12 02:49:02 +0300

    Microsoft IoC container eklenerek generic class ve interfaceler arasındaki bağımlılıklar tanımlandı.

---
+++ BlogScript/BlogScript.Business/Containers/MicrosoftIoC/CustomIoCExtension.cs
@@ -0,0 +1,20 @@
+﻿using BlogScript.Business.Abstract;
+using BlogScript.Business.Concrete;
+using BlogScript.DataAccess.Abstract;
+using BlogScript.DataAccess.Concrete.EFCore.Repositories;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BlogScript.Business.Containers.MicrosoftIoC
+{
+    public static class CustomIoCExtension
+    {
+        public static void AddDependencies(this IServiceCollection services)
+        {
+            services.AddScoped(typeof(IGenericDal<>), typeof(EFGenericRepository<>));
+            services.AddScoped(typeof(IGenericService<>), typeof(GenericManager<>));
+        }
+    }
+}
