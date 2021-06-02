168ccce99ddedb7bb21a480a40a7a2851be570c4
Hakan Tutkun <hakantutkun@gmail.com>
2021-06-03 01:05:02 +0300

    CommentManager ve CategoryManager hataları giderildi.

--- BlogScript/BlogScript.Business/Concrete/CategoryManager.cs
+++ BlogScript/BlogScript.Business/Concrete/CategoryManager.cs
@@ -1,12 +1,14 @@
-﻿using BlogScript.DataAccess.Abstract;
+﻿using BlogScript.Business.Abstract;
+using BlogScript.DataAccess.Abstract;
 using BlogScript.Entities.Concrete;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace BlogScript.Business.Concrete
 {
-    public class CategoryManager : GenericManager<Category>, ICategoryDal
+    public class CategoryManager : GenericManager<Category>, ICategoryService
     {
         private readonly IGenericDal<Category> _genericDal;
         public CategoryManager(IGenericDal<Category> genericDal) : base(genericDal)
--- BlogScript/BlogScript.Business/Concrete/CommentManager.cs
+++ BlogScript/BlogScript.Business/Concrete/CommentManager.cs
@@ -1,11 +1,19 @@
-﻿using System;
+﻿using BlogScript.Business.Abstract;
+using BlogScript.DataAccess.Abstract;
+using BlogScript.Entities.Concrete;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace BlogScript.Business.Concrete
 {
-    public class CommentManager
+    public class CommentManager : GenericManager<Comment>, ICommentService
     {
+        private readonly IGenericDal<Comment> _genericDal;
 
+        public CommentManager(IGenericDal<Comment> genericDal) : base(genericDal)
+        {
+            _genericDal = genericDal;
+        }
     }
 }
