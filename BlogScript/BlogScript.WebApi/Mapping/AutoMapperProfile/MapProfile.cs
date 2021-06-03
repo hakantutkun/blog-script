2f246927984622852565192c692960d2df07dab9
Hakan Tutkun <hakantutkun@gmail.com>
2021-06-04 00:01:11 +0300

    WebApi Projesine AutoMapper eklendi.

---
+++ BlogScript/BlogScript.WebApi/Mapping/AutoMapperProfile/MapProfile.cs
@@ -0,0 +1,33 @@
+﻿using AutoMapper;
+using BlogScript.DTOs.DTOs.BlogDTOs;
+using BlogScript.DTOs.DTOs.CategoryDTOs;
+using BlogScript.Entities.Concrete;
+using BlogScript.WebApi.Models;
+
+namespace BlogScript.WebApi.Mapping.AutoMapperProfile
+{
+    public class MapProfile : Profile
+    {
+        public MapProfile()
+        {
+            CreateMap<BlogListDto, Blog>();
+            CreateMap<Blog, BlogListDto>();
+
+            CreateMap<BlogUpdateModel, Blog>();
+            CreateMap<Blog, BlogUpdateModel>();
+
+            CreateMap<BlogAddModel, Blog>();
+            CreateMap<Blog, BlogAddModel>();
+
+            CreateMap<CategoryAddDto, Category>();
+            CreateMap<Category, CategoryAddDto>();
+
+            CreateMap<CategoryListDto, Category>();
+            CreateMap<Category, CategoryListDto>();
+
+            CreateMap<CategoryUpdateDto, Category>();
+            CreateMap<Category, CategoryUpdateDto>();
+
+        }
+    }
+}
