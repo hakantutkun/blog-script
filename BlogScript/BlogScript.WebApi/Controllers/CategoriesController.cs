37c2106464cf4d3a993cc61e9a19716b00ca03ed
Hakan Tutkun <hakantutkun@gmail.com>
2021-06-06 00:26:51 +0300

    API projesine CategoriesController eklendi.

---
+++ BlogScript/BlogScript.WebApi/Controllers/CategoriesController.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using BlogScript.Business.Abstract;
+using BlogScript.DTOs.DTOs.CategoryDTOs;
+using BlogScript.Entities.Concrete;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlogScript.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriesController : ControllerBase
+    {
+        private readonly ICategoryService _categoryService;
+        private readonly IMapper _mapper;
+
+        public CategoriesController(IMapper mapper, ICategoryService categoryService)
+        {
+            _mapper = mapper;
+            _categoryService = categoryService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            return Ok(_mapper.Map<List<CategoryListDto>>(await _categoryService.GetAllSortedById()));
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetById(int id)
+        {
+            return Ok(_mapper.Map<CategoryListDto>(await _categoryService.FindByIdAsync(id)));
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(CategoryAddDto categoryAddDto)
+        {
+            await _categoryService.AddAsync(_mapper.Map<Category>(categoryAddDto));
+            return Created("", categoryAddDto);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Update(int id, CategoryUpdateDto categoryUpdateDto)
+        {
+            if (id != categoryUpdateDto.Id)
+            {
+                return BadRequest("Geçersiz Id");
+            }
+            // Burda bir hata var deploy bolumunde duzeltiliyor...
+            await _categoryService.UpdateAsync(_mapper.Map<Category>(categoryUpdateDto));
+            return NoContent();
+
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> Delete(int id)
+        {
+            await _categoryService.RemoveAsync(new Category { Id = id });
+            return NoContent();
+        }
+    }
+}
