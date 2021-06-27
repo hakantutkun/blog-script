32a57f9a11ac689c6c06c52da541c6874931ed34
Hakan Tutkun <hakantutkun@gmail.com>
2021-06-27 22:44:48 +0300

    BlogListModel ve BlogApiService eklendi.

---
+++ BlogScript/BlogScript.FrontEnd/ApiServices/Abstract/IBlogApiService.cs
@@ -0,0 +1,13 @@
+﻿using BlogScript.FrontEnd.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlogScript.FrontEnd.ApiServices.Abstract
+{
+    public interface IBlogApiService
+    {
+        Task<List<BlogListModel>> GetAllAsync();
+    }
+}
---
+++ BlogScript/BlogScript.FrontEnd/ApiServices/Concrete/BlogApiManager.cs
@@ -0,0 +1,31 @@
+﻿using BlogScript.FrontEnd.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace BlogScript.FrontEnd.ApiServices.Concrete
+{
+    public class BlogApiManager
+    {
+        private readonly HttpClient _httpClient;
+
+        public BlogApiManager(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+            _httpClient.BaseAddress = new Uri("http://localhost:57175/api/blogs");
+        }
+
+        public async Task<List<BlogListModel>> GetAllAsync()
+        {
+            var responseMessage = await _httpClient.GetAsync("");
+            if(responseMessage.IsSuccessStatusCode)
+            {
+                return JsonConvert.DeserializeObject<List<BlogListModel>>(await responseMessage.Content.ReadAsStringAsync());
+            }
+            return null;
+        }
+    }
+}
