410f94ad0f8e0e8e2ddbb79e5db5adee009ea898
Hakan Tutkun <hakantutkun@gmail.com>
2021-06-28 22:47:27 +0300

    ImagesController eklendi.

---
+++ BlogScript/BlogScript.WebApi/Controllers/ImagesController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BlogScript.Business.Abstract;
+using BlogScript.Entities.Concrete;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlogScript.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ImagesController : ControllerBase
+    {
+        private readonly IBlogService _blogService;
+
+        public ImagesController(IBlogService blogService)
+        {
+            _blogService = blogService;
+        }
+
+        public async Task<IActionResult> GetBlogImageById(int id)
+        {
+            var blog = await _blogService.FindByIdAsync(id);
+            if (string.IsNullOrWhiteSpace(blog.ImagePath))
+                return NotFound("Resim Yok");
+            return File($"/Images/Posts/{blog.ImagePath}", "image/jpeg");
+        }
+    }
+}
